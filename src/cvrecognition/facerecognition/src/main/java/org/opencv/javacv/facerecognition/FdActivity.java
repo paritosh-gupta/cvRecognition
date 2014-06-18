84e6bc40c6833407b29865536bab730f194d6c3a
Siddhanathan Shanmugam <siddhanathan@gmail.com>
2014-06-19 03:33:13 +0530

    Initial push

---
+++ src/cvrecognition/facerecognition/src/main/java/org/opencv/javacv/facerecognition/FdActivity.java
@@ -0,0 +1,570 @@
+package org.opencv.javacv.facerecognition;
+
+import java.io.ByteArrayOutputStream;
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+
+import org.opencv.android.BaseLoaderCallback;
+import org.opencv.android.CameraBridgeViewBase.CvCameraViewFrame;
+
+import org.opencv.android.LoaderCallbackInterface;
+import org.opencv.android.OpenCVLoader;
+import org.opencv.android.Utils;
+import org.opencv.core.Core;
+import org.opencv.core.Mat;
+import org.opencv.core.MatOfRect;
+import org.opencv.core.Rect;
+import org.opencv.core.Scalar;
+import org.opencv.core.Size;
+
+import org.opencv.android.CameraBridgeViewBase.CvCameraViewListener2;
+import org.opencv.objdetect.CascadeClassifier;
+
+import android.app.Activity;
+import android.content.Context;
+import android.content.Intent;
+
+import android.graphics.Bitmap;
+import android.graphics.Canvas;
+import android.os.Bundle;
+
+import android.os.Handler;
+import android.os.Message;
+import android.util.Log;
+import android.view.KeyEvent;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.view.WindowManager;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ImageButton;
+import android.widget.ImageView;
+import android.widget.TextView;
+import android.widget.Toast;
+import android.widget.ToggleButton;
+
+import android.view.MenuInflater;
+import android.app.ActionBar;
+
+
+
+
+
+
+public class FdActivity extends Activity implements CvCameraViewListener2 {
+
+    private static final String    TAG                 = "OCVSample::Activity";
+    private static final Scalar    FACE_RECT_COLOR     = new Scalar(0, 255, 0, 255);
+    public static final int        JAVA_DETECTOR       = 0;
+    public static final int        NATIVE_DETECTOR     = 1;
+    
+    public static final int TRAINING= 0;
+    public static final int SEARCHING= 1;
+    public static final int IDLE= 2;
+    
+    private static final int frontCam =1;
+    private static final int backCam =2;
+
+    public int current_cam = 0;
+    	    		
+    
+    private int faceState=IDLE;
+    private MenuItem               nBackCam;
+    private MenuItem               mFrontCam;
+    
+
+    private Mat                    mRgba;
+    private String                    pName;
+    private Mat                    mGray;
+    public Mat                    face;
+    File                   mCascadeFile;
+    private CascadeClassifier      mJavaDetector;
+
+    int                    mDetectorType       = JAVA_DETECTOR;
+    String[]               mDetectorName;
+
+    private float                  mRelativeFaceSize   = 0.2f;
+    private int                    mAbsoluteFaceSize   = 0;
+    private int mLikely=999;
+    
+    String mPath="";
+
+    private Tutorial3View   mOpenCvCameraView;
+    private int mChooseCamera = backCam;
+    
+    EditText text;
+    TextView textresult;
+    private  ImageView Iv;
+    Bitmap mBitmap;
+    Handler mHandler;
+  
+    PersonRecognizer fr;
+    ToggleButton toggleButtonGrabar,toggleButtonTrain,buttonSearch;
+    ImageView ivGreen,ivYellow,ivRed; 
+    ImageButton imCamera;
+    
+    TextView textState;
+    com.googlecode.javacv.cpp.opencv_contrib.FaceRecognizer faceRecognizer;
+   
+    
+    static final long MAXIMG = 10;
+
+    int[] labels = new int[(int)MAXIMG];
+    int countImages=0;
+    
+    labels labelsFile;
+    
+    
+    private BaseLoaderCallback  mLoaderCallback = new BaseLoaderCallback(this) {
+        @Override
+        public void onManagerConnected(int status) {
+            switch (status) {
+                case LoaderCallbackInterface.SUCCESS:
+                {
+                    Log.i(TAG, "OpenCV loaded successfully");
+                    
+ 
+                    fr=new PersonRecognizer(mPath);
+                    String s = getResources().getString(R.string.Straininig);
+                    Toast.makeText(getApplicationContext(),s, Toast.LENGTH_LONG).show();
+                    fr.load();
+                    
+                    try {
+                        // load cascade file from application resources
+                        InputStream is = getResources().openRawResource(R.raw.lbpcascade_frontalface);
+                        File cascadeDir = getDir("cascade", Context.MODE_PRIVATE);
+                        mCascadeFile = new File(cascadeDir, "lbpcascade.xml");
+                        FileOutputStream os = new FileOutputStream(mCascadeFile);
+
+                        byte[] buffer = new byte[4096];
+                        int bytesRead;
+                        while ((bytesRead = is.read(buffer)) != -1) {
+                            os.write(buffer, 0, bytesRead);
+                        }
+                        is.close();
+                        os.close();
+
+                        mJavaDetector = new CascadeClassifier(mCascadeFile.getAbsolutePath());
+                        if (mJavaDetector.empty()) {
+                            Log.e(TAG, "Failed to load cascade classifier");
+                            mJavaDetector = null;
+                        } else
+                            Log.i(TAG, "Loaded cascade classifier from " + mCascadeFile.getAbsolutePath());
+
+                        cascadeDir.delete();
+
+                    } catch (IOException e) {
+                        e.printStackTrace();
+                        Log.e(TAG, "Failed to load cascade. Exception thrown: " + e);
+                    }
+
+                    mOpenCvCameraView.enableView();
+              
+                } break;
+                default:
+                {
+                    super.onManagerConnected(status);
+                } break;
+                
+                
+            }
+        }
+    };
+
+    public FdActivity() {
+        mDetectorName = new String[2];
+        mDetectorName[JAVA_DETECTOR] = "Java";
+        mDetectorName[NATIVE_DETECTOR] = "Native (tracking)";
+    }
+
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        Log.i(TAG, "called onCreate");
+
+        super.onCreate(savedInstanceState);
+
+        ActionBar actionBar = getActionBar();
+        actionBar.setDisplayHomeAsUpEnabled(true);
+
+        getWindow().addFlags(WindowManager.LayoutParams.FLAG_KEEP_SCREEN_ON);
+
+        setContentView(R.layout.face_detect_surface_view);
+
+        mOpenCvCameraView = (Tutorial3View) findViewById(R.id.tutorial3_activity_java_surface_view);
+   
+        mOpenCvCameraView.setCvCameraViewListener(this);
+       
+        
+        mPath=getFilesDir()+"/facerecogOCV/";
+        		
+        labelsFile= new labels(mPath);
+                 
+        Iv=(ImageView)findViewById(R.id.imageView1);
+        textresult = (TextView) findViewById(R.id.textView1);
+        
+        mHandler = new Handler() {
+            @Override
+            public void handleMessage(Message msg) {
+            	if (msg.obj=="IMG")
+            	{
+            	 Canvas canvas = new Canvas();
+                 canvas.setBitmap(mBitmap);
+                 Iv.setImageBitmap(mBitmap);
+                 if (countImages>=MAXIMG-1)
+                 {
+                	 toggleButtonGrabar.setChecked(false);
+                 	 grabarOnclick();
+                 }
+            	}
+            	else
+            	{
+            		 textresult.setText(msg.obj.toString());
+            		 ivGreen.setVisibility(View.INVISIBLE);
+            	     ivYellow.setVisibility(View.INVISIBLE);
+            	     ivRed.setVisibility(View.INVISIBLE);
+            	     
+            	     if (mLikely<40) {
+
+                         ivGreen.setVisibility(View.VISIBLE);
+                         pName=msg.obj.toString();
+
+                         Intent i = new Intent(org.opencv.javacv.facerecognition.FdActivity.this,
+                                 org.opencv.javacv.facerecognition.notes.class);
+
+                         ByteArrayOutputStream bStream = new ByteArrayOutputStream();
+                         Bitmap bit;
+                         bit = Bitmap.createBitmap(face.width(),face.height(), Bitmap.Config.ARGB_8888);
+                         Utils.matToBitmap(face, bit);
+                         bit.compress(Bitmap.CompressFormat.PNG, 100, bStream);
+                         byte[] byteArray = bStream.toByteArray();
+                         i.putExtra("face",byteArray);
+                         i.putExtra("name",pName);
+                         startActivity(i);
+
+                     }
+
+                     else if (mLikely<60) {
+                         ivYellow.setVisibility(View.VISIBLE);
+
+                         pName=msg.obj.toString();
+
+                         Intent i = new Intent(org.opencv.javacv.facerecognition.FdActivity.this,
+                                 org.opencv.javacv.facerecognition.notes.class);
+
+                         ByteArrayOutputStream bStream = new ByteArrayOutputStream();
+                         Bitmap bit;
+                         bit = Bitmap.createBitmap(face.width(),face.height(), Bitmap.Config.ARGB_8888);
+                         Utils.matToBitmap(face, bit);
+                         bit.compress(Bitmap.CompressFormat.PNG, 100, bStream);
+                         byte[] byteArray = bStream.toByteArray();
+                         i.putExtra("face",byteArray);
+                         i.putExtra("name",pName);
+                         startActivity(i);
+                     }
+            		else if (mLikely<80) {
+                         ivRed.setVisibility(View.VISIBLE);
+                         textresult.setText("");
+                     } else {
+                        textresult.setText("");
+                        ivRed.setVisibility(View.INVISIBLE);
+                        ivGreen.setVisibility(View.INVISIBLE);
+                        ivYellow.setVisibility(View.INVISIBLE);
+                     }
+            	}
+            }
+        };
+        text=(EditText)findViewById(R.id.editText1);
+        toggleButtonGrabar=(ToggleButton)findViewById(R.id.toggleButtonGrabar);
+        buttonSearch=(ToggleButton)findViewById(R.id.buttonBuscar);
+        toggleButtonTrain=(ToggleButton)findViewById(R.id.toggleButton1);
+        textState= (TextView)findViewById(R.id.textViewState);
+        ivGreen=(ImageView)findViewById(R.id.imageView3);
+        ivYellow=(ImageView)findViewById(R.id.imageView4);
+        ivRed=(ImageView)findViewById(R.id.imageView2);
+        imCamera=(ImageButton)findViewById(R.id.imageButton1);
+        
+        ivGreen.setVisibility(View.INVISIBLE);
+        ivYellow.setVisibility(View.INVISIBLE);
+        ivRed.setVisibility(View.INVISIBLE);
+        text.setVisibility(View.INVISIBLE);
+        textresult.setVisibility(View.INVISIBLE);
+    
+
+      
+        toggleButtonGrabar.setVisibility(View.INVISIBLE);
+        
+        
+        text.setOnKeyListener(new View.OnKeyListener() {
+        	public boolean onKey(View v, int keyCode, KeyEvent event) {
+        		if ((text.getText().toString().length()>0)&&(toggleButtonTrain.isChecked()))
+        			toggleButtonGrabar.setVisibility(View.VISIBLE);
+        		else
+        			toggleButtonGrabar.setVisibility(View.INVISIBLE);
+        		
+                return false;
+        	}
+        });
+			
+
+        
+		toggleButtonTrain.setOnClickListener(new View.OnClickListener() {
+			public void onClick(View v) {
+				if (toggleButtonTrain.isChecked()) {
+					textState.setText(getResources().getString(R.string.SEnter));
+					buttonSearch.setVisibility(View.INVISIBLE);
+					textresult.setVisibility(View.VISIBLE);
+					text.setVisibility(View.VISIBLE);
+					textresult.setText(getResources().getString(R.string.SFaceName));
+					if (text.getText().toString().length() > 0)
+						toggleButtonGrabar.setVisibility(View.VISIBLE);
+					
+
+					ivGreen.setVisibility(View.INVISIBLE);
+					ivYellow.setVisibility(View.INVISIBLE);
+					ivRed.setVisibility(View.INVISIBLE);
+					
+
+				} else {
+					textState.setText(R.string.Straininig); 
+					textresult.setText("");
+					text.setVisibility(View.INVISIBLE);
+					
+					buttonSearch.setVisibility(View.VISIBLE);
+					textresult.setText("");
+					{
+						toggleButtonGrabar.setVisibility(View.INVISIBLE);
+						text.setVisibility(View.INVISIBLE);
+					}
+			        Toast.makeText(getApplicationContext(),getResources().getString(R.string.Straininig), Toast.LENGTH_LONG).show();
+					fr.train();
+					textState.setText(getResources().getString(R.string.SIdle));
+
+				}
+			}
+
+		});
+        
+     
+
+        toggleButtonGrabar.setOnClickListener(new View.OnClickListener() {
+
+			public void onClick(View v) {
+				grabarOnclick();
+			}
+		});
+        
+        buttonSearch.setOnClickListener(new View.OnClickListener() {
+
+     			public void onClick(View v) {
+     				if (buttonSearch.isChecked())
+     				{
+     					if (!fr.canPredict())
+     						{
+     						buttonSearch.setChecked(false);
+     			            Toast.makeText(getApplicationContext(), getResources().getString(R.string.SCanntoPredic), Toast.LENGTH_LONG).show();
+     			            return;
+     						}
+     					textState.setText(getResources().getString(R.string.SSearching));
+     					toggleButtonGrabar.setVisibility(View.INVISIBLE);
+     					toggleButtonTrain.setVisibility(View.INVISIBLE);
+     					text.setVisibility(View.INVISIBLE);
+     					faceState=SEARCHING;
+     					textresult.setVisibility(View.VISIBLE);
+     				}
+     				else
+     				{
+     					faceState=IDLE;
+     					textState.setText(getResources().getString(R.string.SIdle));
+     					toggleButtonGrabar.setVisibility(View.INVISIBLE);
+     					toggleButtonTrain.setVisibility(View.VISIBLE);
+     					text.setVisibility(View.INVISIBLE);
+     					textresult.setVisibility(View.INVISIBLE);
+     					
+     				}
+     			}
+     		});
+        
+        boolean success=(new File(mPath)).mkdirs();
+        if (!success)
+        {
+        	Log.e("Error","Error creating directory");
+        }
+    }
+    
+    void grabarOnclick()
+    {
+    	if (toggleButtonGrabar.isChecked())
+			faceState=TRAINING;
+			else
+			{
+			  countImages=0;
+			  faceState=IDLE;
+			}
+		
+
+    }
+    
+    @Override
+    public void onPause()
+    {
+        super.onPause();
+        if (mOpenCvCameraView != null)
+            mOpenCvCameraView.disableView();       
+    }
+
+    @Override
+    public void onResume()
+    {
+        super.onResume();
+        OpenCVLoader.initAsync(OpenCVLoader.OPENCV_VERSION_2_4_3, this, mLoaderCallback);
+       
+      	
+    }
+
+    public void onDestroy() {
+        super.onDestroy();
+        mOpenCvCameraView.disableView();
+    }
+
+    public void onCameraViewStarted(int width, int height) {
+        mGray = new Mat();
+        mRgba = new Mat();
+    }
+
+    public void onCameraViewStopped() {
+        mGray.release();
+        mRgba.release();
+    }
+
+    public Mat onCameraFrame(CvCameraViewFrame inputFrame) {
+
+        mRgba = inputFrame.rgba();
+        mGray = inputFrame.gray();
+
+        if (mAbsoluteFaceSize == 0) {
+            int height = mGray.rows();
+            if (Math.round(height * mRelativeFaceSize) > 0) {
+                mAbsoluteFaceSize = Math.round(height * mRelativeFaceSize);
+            }
+        }
+
+        MatOfRect faces = new MatOfRect();
+
+        if (mDetectorType == JAVA_DETECTOR) {
+            if (mJavaDetector != null)
+                mJavaDetector.detectMultiScale(mGray, faces, 1.1, 2, 2, // TODO: objdetect.CV_HAAR_SCALE_IMAGE
+                        new Size(mAbsoluteFaceSize, mAbsoluteFaceSize), new Size());
+        }
+        else {
+            Log.e(TAG, "Detection method is not selected!");
+        }
+
+        Rect[] facesArray = faces.toArray();
+        
+        if ((facesArray.length==1)&&(faceState==TRAINING)&&(countImages<MAXIMG)&&(!text.getText().toString().isEmpty()))
+        {
+        
+
+        Mat m=new Mat();
+        Rect r=facesArray[0];
+       
+        
+        m=mRgba.submat(r);
+        mBitmap = Bitmap.createBitmap(m.width(),m.height(), Bitmap.Config.ARGB_8888);
+        
+        
+        Utils.matToBitmap(m, mBitmap);
+        
+        Message msg = new Message();
+        String textTochange = "IMG";
+        msg.obj = textTochange;
+        mHandler.sendMessage(msg);
+        if (countImages<MAXIMG)
+        {
+        	fr.add(m, text.getText().toString());
+        	countImages++;
+        }
+
+        }
+        else
+        	 if ((facesArray.length>0)&& (faceState==SEARCHING))
+          {
+        	  Mat m=new Mat();
+        	  m=mGray.submat(facesArray[0]);
+              face=mRgba.submat(facesArray[0]);
+        	  mBitmap = Bitmap.createBitmap(m.width(),m.height(), Bitmap.Config.ARGB_8888);
+        
+             
+              Utils.matToBitmap(m, mBitmap);
+              Message msg = new Message();
+              String textTochange = "IMG";
+              msg.obj = textTochange;
+              mHandler.sendMessage(msg);
+        	  
+              textTochange=fr.predict(m);
+              mLikely=fr.getProb();
+        	  msg = new Message();
+        	  msg.obj = textTochange;
+        	  mHandler.sendMessage(msg);
+        	  
+          }
+        for (int i = 0; i < facesArray.length; i++)
+            Core.rectangle(mRgba, facesArray[i].tl(), facesArray[i].br(), FACE_RECT_COLOR, 3);
+
+        return mRgba;
+    }
+
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.main_activity_actions, menu);
+        current_cam = 2;
+        return super.onCreateOptionsMenu(menu);
+    }
+
+    public Mat getFace(){
+        return face;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+
+        // Handle presses on the action bar items
+        switch (item.getItemId()) {
+            case R.id.action_camera:
+                if(current_cam == 1) {
+                    mChooseCamera=backCam;
+                    mOpenCvCameraView.setCamBack();
+                    current_cam = 2;
+                } else {
+                    mChooseCamera=frontCam;
+                    mOpenCvCameraView.setCamFront();
+                    current_cam = 1;
+                }
+                break;
+            case R.id.action_settings:
+                current_cam = current_cam;
+                break;
+            case R.id.action_gallery:
+                Intent i = new Intent(org.opencv.javacv.facerecognition.FdActivity.this,
+                        org.opencv.javacv.facerecognition.ImageGallery.class);
+                i.putExtra("path", mPath);
+                startActivity(i);
+                break;
+            case R.id.action_write:
+                Intent t = new Intent(org.opencv.javacv.facerecognition.FdActivity.this,
+                        org.opencv.javacv.facerecognition.input.class);
+                startActivity(t);
+                break;
+            default:
+                return super.onOptionsItemSelected(item);
+        }
+        return true;
+    }
+
+}
---
+++ src/cvrecognition/facerecognition/src/main/java/org/opencv/javacv/facerecognition/notes.java
@@ -0,0 +1,206 @@
+package org.opencv.javacv.facerecognition;
+
+
+
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+
+import org.opencv.android.BaseLoaderCallback;
+import org.opencv.android.CameraBridgeViewBase.CvCameraViewFrame;
+
+import org.opencv.android.LoaderCallbackInterface;
+import org.opencv.android.OpenCVLoader;
+import org.opencv.android.Utils;
+import org.opencv.core.Core;
+import org.opencv.core.Mat;
+import org.opencv.core.MatOfRect;
+import org.opencv.core.Rect;
+import org.opencv.core.Scalar;
+import org.opencv.core.Size;
+
+import org.opencv.android.CameraBridgeViewBase.CvCameraViewListener2;
+import org.opencv.objdetect.CascadeClassifier;
+
+import android.app.Activity;
+import android.content.Context;
+import android.content.Intent;
+
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.graphics.Canvas;
+import android.os.Bundle;
+
+import android.os.Handler;
+import android.os.Message;
+import android.os.Parcelable;
+import android.util.Log;
+import android.view.KeyEvent;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.view.Window;
+import android.view.WindowManager;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ImageButton;
+import android.widget.ImageView;
+import android.widget.TextView;
+import android.widget.Toast;
+import android.widget.ToggleButton;
+
+import android.view.MenuInflater;
+import android.app.ActionBar;
+
+
+import static  com.googlecode.javacv.cpp.opencv_highgui.*;
+import static  com.googlecode.javacv.cpp.opencv_core.*;
+
+import static  com.googlecode.javacv.cpp.opencv_imgproc.*;
+import static com.googlecode.javacv.cpp.opencv_contrib.*;
+
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.FilenameFilter;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.util.ArrayList;
+
+import org.opencv.android.Utils;
+import org.opencv.core.Mat;
+
+import com.googlecode.javacv.cpp.opencv_imgproc;
+import com.googlecode.javacv.cpp.opencv_contrib.FaceRecognizer;
+import com.googlecode.javacv.cpp.opencv_core.IplImage;
+import com.googlecode.javacv.cpp.opencv_core.MatVector;
+
+import android.app.Activity;
+import android.graphics.Bitmap;
+import android.os.Environment;
+import android.util.Log;
+import android.widget.AdapterView;
+import android.widget.Toast;
+import android.os.Bundle;
+import android.widget.TextView;
+import android.widget.ViewSwitcher;
+
+
+
+
+
+/**
+ * Created by paritosh on 6/1/14.
+ */
+
+public class notes extends Activity {
+
+    private static final Scalar    FACE_RECT_COLOR     = new Scalar(0, 255, 0, 255);
+
+    private ImageView Iv;
+
+    private String fName;
+
+    private TextView textFill;
+    private TextView relationFill;
+    private EditText notesFill; // fill notes
+    private TextView noteR; // read notes
+
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        this.setContentView(R.layout.second);
+         Bitmap bmp;
+
+        byte[] byteArray = getIntent().getByteArrayExtra("face");
+        bmp = BitmapFactory.decodeByteArray(byteArray, 0, byteArray.length);
+        Bundle extras = getIntent().getExtras();
+        String userName=new String();
+        if (extras != null) {
+            userName = extras.getString("name");
+        }
+
+        textFill = (TextView) findViewById(R.id.name);
+        textFill.setText(userName);
+        fName=userName;
+        relationFill = (TextView) findViewById(R.id.relation);
+        EditText notesFill = (EditText) findViewById(R.id.notes);
+        noteR = (TextView) findViewById(R.id.noteRead);
+        Iv=(ImageView) findViewById(R.id.face);
+        Iv.setImageBitmap(bmp);
+        String abc=new String();
+
+        try {
+            //Toast.makeText(getApplicationContext(),"file eader", Toast.LENGTH_SHORT).show();
+               InputStream inputStream = openFileInput(userName + ".txt");
+
+                if ( inputStream != null ) {
+                    InputStreamReader inputStreamReader = new InputStreamReader(inputStream);
+                    BufferedReader bufferedReader = new BufferedReader(inputStreamReader);
+                    String receiveString = "";
+                    StringBuilder stringBuilder = new StringBuilder();
+
+                    //-------------------relationship ship incase needs to be added
+
+                    //receiveString = bufferedReader.readLine();
+                    //relationFill.setText(receiveString);
+
+                    //-------------------
+                    while ( (receiveString = bufferedReader.readLine()) != null )
+                        {
+                           stringBuilder.append(receiveString);
+                        //   Toast.makeText(getApplicationContext(),"s =" + stringBuilder.toString(), Toast.LENGTH_SHORT).show();
+                        }
+                    inputStream.close();
+                    noteR.setText(stringBuilder.toString() +noteR.getText().toString());
+                    //Toast.makeText(getApplicationContext(),"noteR set", Toast.LENGTH_SHORT).show();
+
+                }
+            }
+            catch (FileNotFoundException e) {
+              //  Toast.makeText(getApplicationContext(),"f not found", Toast.LENGTH_SHORT).show();
+                Log.e("login activity", "File not found: " + e.toString());
+            } catch (IOException e) {
+                //Toast.makeText(getApplicationContext(),"cannot read file", Toast.LENGTH_SHORT ). show();
+                Log.e("login activity", "Can not read file: " + e.toString());
+            }
+
+
+
+        //text.setText("Name=");
+        //setContentView(text);
+       // Mat Dface=new Mat();
+        //Utils.bitmapToMat(bmp,Dface);
+        //requestWindowFeature(Window.FEATURE_NO_TITLE);
+        //setContentView(R.layout.catalog_view);
+        //Canvas canvas = new Canvas();
+        //canvas.setBitmap(bmp);
+          //   Iv=(ImageView)findViewById(R.id.imageView1);
+        //Iv.setImageBitmap(bmp);*/
+
+    }
+
+    private void writeToFile(String data) {
+//        Toast.makeText(getApplicationContext(),"asdasd", Toast.LENGTH_SHORT).show();
+        try {
+            //Toast.makeText(getApplicationContext(),"in try", Toast.LENGTH_SHORT).show();
+            OutputStreamWriter outputStreamWriter = new OutputStreamWriter(openFileOutput(fName+".txt", Context.MODE_PRIVATE));
+            outputStreamWriter.append(data);
+            outputStreamWriter.close();
+            Toast.makeText(getApplicationContext(),"data written", Toast.LENGTH_SHORT).show();
+        }
+        catch (IOException e) {
+            //Toast.makeText(getApplicationContext(),"write failed", Toast.LENGTH_LONG).show();
+            Log.e("Exception", "File write failed: " + e.toString());
+        }
+    }
+
+    public void writer(View view) {
+        EditText notesFill = (EditText) findViewById(R.id.notes);
+        writeToFile(notesFill.getText().toString());
+
+    }
+
+}
+
