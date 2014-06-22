67eecd2cbea64dbc9b59a2b1284c50573ab48f52
Siddhanathan Shanmugam <siddhanathan@gmail.com>
2014-06-22 08:32:24 +0530

    Added copyrights

--- src/cvrecognition/facerecognition/src/main/java/org/opencv/javacv/facerecognition/notes.java
+++ src/cvrecognition/facerecognition/src/main/java/org/opencv/javacv/facerecognition/notes.java
@@ -1,3 +1,22 @@
+
+
+/*
+ * Copyright (C) 2014 Siddhanathan Shanmugam <siddhanathan@gmail.com>, Paritosh Gupta <paritosh.gupta@yahoo.co.uk>
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>
+ */
+
 package org.opencv.javacv.facerecognition;
 
 
@@ -88,12 +107,6 @@ import android.widget.TextView;
 import android.widget.ViewSwitcher;
 
 
-
-
-
-/**
- * Created by paritosh on 6/1/14.
- */
 
 public class notes extends Activity {
 
